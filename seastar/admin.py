d8ba126ce38032b1720820026fd3bcf7da676fc2
FrancoECeballos <franco.ceballos2005@gmail.com>
2023-09-01 11:45:37 -0300

    Más progreso del classDiagram

--- seastar/admin.py
+++ seastar/admin.py
@@ -4,3 +4,10 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Navio,)
+
+class NavioAdmin(admin.ModelAdmin):
+    def get_navio_name(self,obj):
+        return obj.Navio.nombreNavio
+    search_fields = ('nombreNavio','codigoNavio')
+    list_display = ('nombreNavio','codigoNavio')
+    list_filter = ('nombreNavio','codigoNavio',)
