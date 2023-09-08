1797b79bed8960f11056df13572cd3737927d5c5
Tu Nombre <you@example.com>
2023-09-08 09:50:46 -0300

    seastar eliminado

--- seastar/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .forms import CrearTripulante
-# Create your views here.
-def Home():
-    pass
-
-def crearTripulantes(request):
-    return render(request, "index.html", { "form" : CrearTripulante})
