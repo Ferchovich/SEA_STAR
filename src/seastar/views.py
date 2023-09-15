96cbac1b18d6f348be8cf7492b9c4fc4c80644d4
FrancoECeballos <franco.ceballos2005@gmail.com>
2023-09-15 10:12:24 -0300

    Añadí navio.html

--- src/seastar/views.py
+++ src/seastar/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from .forms import CrearTripulante
-from seastar.models import Tripulante
+from seastar.models import Tripulante, Navio
 # Create your views here.
 
 def home(request):
     return render(request, "./seastar/home.html", {})
 
 def crearTripulantes(request):
-    tripulante = Tripulante.objects.all()
-    return render(request, "./  base.html", { "form" : CrearTripulante})
+    tripulantes = Tripulante.objects.all()
+    return render(request, "./seastar/base.html", { "form" : CrearTripulante})
+
+def verNavios(request):
+    navios = Navio.objects.all()
+    return render(request, "./seastar/navio.html", { "form" : verNavios})
