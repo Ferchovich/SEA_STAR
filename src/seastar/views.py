044d7b3fb212d5877b878ab684cf71bdfca26dc7
FrancoECeballos <franco.ceballos2005@gmail.com>
2023-10-19 10:26:03 -0300

    Remodelación completa del template (Excepto del Store), con listas de navios e Itinerarios

--- src/seastar/views.py
+++ src/seastar/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
-from seastar.models import Tripulante, Navio
+from django.views.generic.list import ListView
+from seastar.models import Tripulante, Navio, Itinerario, Puerto
 # Create your views here.
 
 def home(request):
     return render(request, "./index.html", {})
 
 def about(request):
-    return render(request, "./about.html")
+    itinerarios = Itinerario.objects.all()
+    puertos = Puerto.objects.all()
+    return render(request, "./about.html", { "itinerarios" : itinerarios ,  "puertos" : puertos })
 
 def products(request):
     navios = Navio.objects.all()
-    return render(request, "./products.html", { "navios" : navios})
+    return render(request, "./products.html", { "navios" : navios })
 
 def store(request):
     return render(request, "./store.html")
 
 def login(request):
     login = Navio.objects.all()
-    return render(request, "./login.html", { "login" : login})
+    return render(request, "./login.html", { "login" : login })
 
